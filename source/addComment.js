39ac457af27a789ead76d2204f9ef0f5e94a7f69
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-12 14:38:42 -0600

    Implements comment adding functionality

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-  // add your code here
+  let newCommentButton  = $('#new_comment_button');
+  let newCommentForm    = $('#new_comment');
+  let commentList       = $('#comment_list');
+
+  newCommentButton.click(function() {
+    newCommentButton.addClass('hidden');
+    newCommentForm.removeClass('hidden');
+  });
+
+  newCommentForm.submit(function(event) {
+    event.preventDefault();
+    newCommentButton.removeClass('hidden');
+    newCommentForm.addClass('hidden');
+    addComment($('#comment_text').val(), $('#author_name').val());
+  });
+
+  function addComment(content, author) {
+    if (!content) {
+      alert('You must add something in the comment text.')
+      return;
+    }
+    let newComment = document.createElement('li')
+    commentList.append(newComment);
+    $('#comment_list li:last-child').text(content);
+
+    let newAuthor = document.createElement('span')
+    $('#comment_list li:last-child').append(newAuthor);
+    $('#comment_list li:last-child span').addClass('author');
+    $('#comment_list li:last-child span').text(author)
+  }
 })
